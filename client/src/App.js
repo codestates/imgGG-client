144d1e0553d008c654be9eb234221944fecfa12f
Jaeyoung Paik <jaeyoungpaik@Jaeyoungui-MacBookPro.local>
2020-11-11 09:41:59 +0900

    built basic part b4 coding

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import Signin from "./pages/Signin";
+// import Signup from "./pages/Signup";
+// import Mypage from "./pages/Mypage";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    );
+  }
 }
 
 export default App;
