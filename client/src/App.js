28b981115afb283f43a89dc05fe7ed7ad0d1ebfd
Jaeyoung Paik <jaeyoungpaik@Jaeyoungui-MacBookPro.local>
2020-11-13 14:59:19 +0900

    completed add tag and save at state

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Mainpage from './components/Mainpage';
 import Mypage from './components/mypage/Mypage';
 import { Route, Switch } from 'react-router-dom';
 import Upload from './components/Upload';
+import "./App.css";
 class App extends Component {
   render() {
     return (
--- client/src/components/Upload.js
+++ client/src/components/Upload.js
@@ -2,71 +2,89 @@ import React from "react";
 import "./Upload.css";
 import Nav from './Nav';
 import axios from 'axios'
-import { Link } from 'react-router-dom';
 import { IMGBB_API_KEY } from '../config/config';
 
 
 class Upload extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          file : '',
-          previewURL : '',
-          img : false
-        }
-    this.handleInputValue = this.handleInputValue.bind(this);
-
-    this.handleSubmitImg = this.handleSubmitImg.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: '',
+      previewURL: '',
+      tag: null,
+      alltag: [],
+      img: false
     }
+  }
 
-    handleInputValue = (event) => {
-        let uploadImage = (img) => {
-          let body = new FormData()
-          body.set('key', IMGBB_API_KEY)
-          body.append('image', img)
-      
-          return (axios({
-            method: 'post',
-            url: 'https://api.imgbb.com/1/upload',
-            data: body
-          }))
-        }
-        uploadImage(event.target.files[0])
-        .then(resp => {
-          this.setState({file : resp,
-          previewURL : resp.data.data.display_url,
-          img : true})
+  handleInputValue = (event) => {
+    let uploadImage = (img) => {
+      let body = new FormData()
+      body.set('key', IMGBB_API_KEY)
+      body.append('image', img)
+
+      return (axios({
+        method: 'post',
+        url: 'https://api.imgbb.com/1/upload',
+        data: body
+      }))
+    }
+    uploadImage(event.target.files[0])
+      .then(resp => {
+        this.setState({
+          file: resp,
+          previewURL: resp.data.data.display_url,
+          img: true
         })
-      };
+      })
+  };
+
+  handleSubmitImg = () => {
+    const { file, previewURL, alltag, img } = this.state
+    const imgInfo = { file, previewURL, alltag, img };
+    console.log(imgInfo)
     
-    handleSubmitImg = () => { 
-      const {file,previewURL,img} = this.state
-        const imgInfo = {file,previewURL,img};
-        console.log(imgInfo)
-    }
+    window.location.reload();
+  }
 
-    render(){
+  handleAddTag = (e) => {
+    this.setState({ tag: e.target.value });
+    console.log(this.state);
+  }
+
+  handleAddAllTag = () => {
+    const tag = this.state.tag
+    const alltag = this.state.alltag;
+    alltag.push(tag);
+    this.setState({ tag: null, alltag: alltag })
+    let input = document.querySelector(".tag-input");
+    input.value = '';
+    console.log(this.state);
+  }
+
+  render() {
     return (<div>
-        <Nav />
-        <div className="upload-box">
-
-          <input type="file" name="file" accept="image/*" onChange={this.handleInputValue} className="input-btn"></input>
-            <div className="preview-box">    
-                {this.state.file.length !==0 ? (<img className="preview" src={this.state.previewURL}></img>) : (<div>사진 미리보기</div>)}
-            </div>
-            <div className="upload-btn">
-              <button type='submit' onClick={this.handleSubmitImg}>업로드</button>
-            </div>
-            <div className="gotoMain">
-              <Link to='/'>메인페이지로</Link>
-            </div>
+      <Nav />
+      <div className="upload-box">
+        <div className="preview-box">
+          {this.state.file.length !== 0 ? (<img className="preview" src={this.state.previewURL}></img>) : (<div className='pic-preview'>사진 미리보기</div>)}
         </div>
         <div className="upload-info">
-          태그: <input></input>
-          <button>추가</button>
+          <input type="file" name="file" accept="image/*" onChange={this.handleInputValue} className="input-btn"></input>
+          <input className="tag-input" onChange={this.handleAddTag} placeholder="태그입력"></input>
+          <button type='submit' onClick={this.handleAddAllTag}>추가</button>
+          <ul className='all-tag'>
+            {(this.state.alltag).map(v => (
+              <div key={v} className="tag-form">{v}</div>
+            ))}
+          </ul>
+          <div className="upload-btn">
+            <button type='submit' onClick={this.handleSubmitImg}>업로드</button>
+          </div>
         </div>
+      </div>
     </div>)
-    }
+  }
 }
 
 export default Upload;
