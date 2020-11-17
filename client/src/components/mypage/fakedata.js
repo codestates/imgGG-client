fe3ab609964cac99a5bdfe069e2b0ee433cb6a69
limdonghyun88 <ehdrhzle3@gmail.com>
2020-11-17 17:56:46 +0900

    app.css search.css

--- client/src/components/mypage/fakedata.js
+++ client/src/components/mypage/fakedata.js
@@ -1,25 +1,25 @@
 export const fakeData = [
     {
         id: 1,
-        url: "https://i.ibb.co/pKMm03Z/1.png",
+        url: "https://i.ibb.co/2qnHR2b/1.jpg",
         alltag: ['abc','b','132', 'caq'],
         like: 5,
     },
     {
         id: 2,
-        url: "https://i.ibb.co/bW3gCTj/spiffygif-46x46.gif",
+        url: "https://i.ibb.co/vVk120T/2.jpg",
         alltag: ['abc','eeee','2111'],
         like: 4,
     },
     {
         id: 3,
-        url: "https://i.ibb.co/phzQLmy/2020-11-12-7-10-17.png",
+        url: "https://i.ibb.co/YNJPphn/3.jpg",
         alltag: ['3412','cee', 'eeee'],
         like: 6,
     },
     {
         id: 4,
-        url: "https://i.ibb.co/bzZqBb7/2.png",
+        url: "https://i.ibb.co/wSBcwZB/4.jpg",
         alltag: ['132','2111','34123'],
         like: 2,
     },
