3ac874b01e4bad9367707cf0471f48e458c5b3f4
mjs0818 <tjfaudwo09@gmail.com>
2020-11-18 23:04:59 +0900

    modify like button

--- client/src/components/Imginfo.js
+++ client/src/components/Imginfo.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import "./Imginfo.css"
 import axios from 'axios';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faThumbsUp} from "@fortawesome/free-regular-svg-icons";
+import {faThumbsUp} from "@fortawesome/free-solid-svg-icons";
+import cookie from 'react-cookies';
 
+const token = cookie.load('token');
 
 class Imginfo extends Component {
   constructor(props){
@@ -12,23 +14,36 @@ class Imginfo extends Component {
       imginfo: '',
       isLike: false,
       likeCount: '',
+      isLogin: false,
+      userId: '',
     }
   }
 
   componentDidMount() {
+    if(token) {
+      axios.get('http://localhost:4000/user/info',
+      { withCredentials: true})
+      .then(result => {
+        this.setState({
+          isLogin: true,
+          userId: result.data.id
+        })
+      });
+    }
     axios.get('http://localhost:4000'+this.props.location.pathname,
     { withCredentials: true }
     )
     .then((result) => {
-      console.log(result);
       this.setState({
         imginfo: result.data,
         likeCount: result.data.like_count
       });
+      console.log('이미지정보',this.state.imginfo);
     })
   };
   
   handleLike = () => {
+    if(token){
     axios.post('http://localhost:4000/image/like',
     {imageId: this.state.imginfo.id},
     {withCredentials: true})
@@ -44,11 +59,10 @@ class Imginfo extends Component {
       })
     }})
   }
+  }
 
   render() {
-
-    console.log(this.props)
-
+    console.log(this.props);
     return (
       <div>
           <div className="info-box">
@@ -66,8 +80,13 @@ class Imginfo extends Component {
               </>
               : ''}
             </div>
-            <div>
-              <button className="like-btn" onClick={this.handleLike}><FontAwesomeIcon className="icon" icon={faThumbsUp}/>{this.state.likeCount}</button>
+            <div className="wrap">
+            <div className="like-btn" onClick={this.handleLike}>
+              {/* <button className="like-btn" onClick={this.handleLike}> */}
+                <FontAwesomeIcon className="icon" color="white" icon={faThumbsUp} size="2x"/>
+                {this.state.likeCount}
+                {/* </button> */}
+            </div>
             </div>
             <div className="tags">태그</div>
             {this.state.imginfo.tags ? this.state.imginfo.tags.map(v => (
